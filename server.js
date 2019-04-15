d90040c34d03cddfa4367d5e197e65bde6852e21
HandOver <diga@oihandover.com>
2019-04-15 13:26:46 -0300

    god

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 app.use(cors())
 
 app.get('/', (req, res) => {
-  return res.send('JESUS LOVES YOU!!')
+  return res.send('JESUS LOVES YOU!! and GOD too')
 })
 
 app.get('/test', (req, res) => {
